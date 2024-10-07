75c1b67e827b91cb9804c43d278e6612548d1fe6
Soumik Mukherjee <101049244+soumikjee@users.noreply.github.com>
2024-10-07 21:17:29 +0530

    Add files via upload

---
+++ JAVA 1 Basics/D.java
@@ -0,0 +1,11 @@
+
+//Join Class Method In Another Class 
+
+public class D {
+    public static void main(String[] args) {
+        System.err.print(" My Name Is : ");
+        Cjoin.s1();
+        System.err.println("I Study In J Spider");
+        
+    }
+}
